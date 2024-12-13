ad24e0e17c95e9b742852c339c07f2292d939c49
Cristian Yancis <bernardoyancis@gmail.com>
2024-12-13 17:42:36 -0600

    selint

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+const { Linter } =require ('eslint');
+
+/** @type {Linter.FlatConfig[]} */
+module.exports [
+  {
+    // Define el entorno y las opciones del parser
+    languageOptions: {
+      ecmaVersion: 2024,
+    },
+    // Define el entorno global
+    env: {
+      es6: true,
+      node: true,
+      mocha: true,
+    },
+    // Extiende las configuraciones recomendadas
+    extends: ['eslint:recommended', 'prettier'],
+    // Configura reglas personalizadas
+    rules: {
+      'no-console': 'warn',
+    },
+  }
+]
